165bac3463e7a1528fe87143d01383342e2560b3
Subash KC <subashkc1@gmail.com>
2018-06-14 16:33:58 +1000

    cap3 syntax changes in lib/capistrano-s3-copy/recipe/s3_copy.rb

--- lib/capistrano-s3-copy/recipes/s3_copy.rb
+++ lib/capistrano-s3-copy/recipes/s3_copy.rb
@@ -1,15 +1,16 @@
-Capistrano::Configuration.instance(false).load do
+require 'capistrano/dsl'
 
-  namespace :s3_copy do
+include Capistrano::DSL
 
-    desc "Internal hook that updates the aws_install.sh script to latest if the deploy completed"
-    task :store_aws_install_script_on_success do
-      cmd = fetch(:s3_copy_aws_install_cmd)
-      if cmd
-        run_locally(cmd)
-      end
+namespace :s3_copy do
+
+  desc "Internal hook that updates the aws_install.sh script to latest if the deploy completed"
+  task :store_aws_install_script_on_success do
+    cmd = fetch(:s3_copy_aws_install_cmd)
+    if cmd
+      run_locally(cmd)
     end
   end
-
-  after 'deploy', 's3_copy:store_aws_install_script_on_success'
 end
+
+after 'deploy', 's3_copy:store_aws_install_script_on_success'
